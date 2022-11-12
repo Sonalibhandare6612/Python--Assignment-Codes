c3f04cc6e79286276a54e27478f147f97a6c364f
Swapnali Bhandare <106583512+Sonalibhandare6612@users.noreply.github.com>
2022-11-12 21:34:26 +0530

    initial commit

---
+++ pyfristpro.py
@@ -0,0 +1,19 @@
+
+    # Taking Input From User And Salary Comparision #
+x = input("Enter Candidate1 Name :")
+y = input("Enter Company Name :")
+z = float(input("Enter Salary of 1:"))
+a = input("Enter Candidate2 Name :")
+b = input("Enter Company Name :")
+c = float(input("Enter Salary of 2 :"))
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+print("This person has greater salary")
+if z > c:
+      print(x)
+else:
+     print(a)
